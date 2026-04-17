c512f17d922a18220cb7ecc2c1a25d60975684fe
memi1252 <jj0475442@gmail.com>
2026-04-17 11:51:50 +0900

    WeaponSelectUI배치

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -1,12 +1,19 @@
 using System;
+using Fusion;
 using UnityEngine;
 
-public class WeaponManager : MonoBehaviour
+public class WeaponManager : NetworkBehaviour
 {
     public static WeaponManager Instance { get; private set; }
 
+    public GameObject weaponUI;
+    public GameObject weaponSelectPrefab;
+    public Transform weaponSelectPanel;
+    
     [Header("무기종류")] 
     public WeaponScriptableObject[] weaponSOs;
+
+    public bool isWeaponSelectActive = false;
     
     private void Awake()
     {
@@ -18,5 +25,40 @@ public class WeaponManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    void Start()
+    {
+        weaponUI.SetActive(false);
+    }
+
+    public override void Spawned()
+    {
+        base.Spawned();
+    }
+
+    public override void Render()
+    {
+        base.Render();
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if (Input.GetKeyDown(KeyCode.M) && Runner.IsSceneAuthority)
+        {
+            WeaponSelect(); // 무기 선택
+        }
+    }
+
+    public void WeaponSelect()
+    {
+        if (isWeaponSelectActive)
+        {
+            return;
+        }
+
+        isWeaponSelectActive = true;
+        
+        
     }
 }
