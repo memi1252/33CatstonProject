a64327e384e627f2270122cab772c385a495b7b4
memi1252 <jj0475442@gmail.com>
2026-05-07 14:31:39 +0900

    드론적 얼추 완성

--- Assets/Scripts/Enemy/AbandonedDroneEnemy.cs
+++ Assets/Scripts/Enemy/AbandonedDroneEnemy.cs
@@ -1,10 +1,16 @@
+using System.Collections;
 using Projectiles.NetworkObjectExample;
 using UnityEngine;
+using UnityEngine;
+using Fusion;
+using Fusion.Addons.Physics;
+using Projectiles.NetworkObjectExample;
 
 public class AbandonedDroneEnemy : Enemy
 {
+    public int attackCount = 4;
     public Transform firePoint;
-    public GameObject bulletPrefab;
+    public PhysicsProjectile bulletPrefab;
     public ParticleSystem fireParticles;
     public TargetAttribute targetAttribute;
     public LayerMask raycastLayerMask;
@@ -12,9 +18,19 @@ public class AbandonedDroneEnemy : Enemy
     protected override void PerformAttack()
     {
         base.PerformAttack();
-        var bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        var physicsProjectile = bullet.GetComponent<PhysicsProjectile>();
-        physicsProjectile.Fire(firePoint.position, firePoint.rotation, enemyData.damage, raycastLayerMask, targetAttribute);
-        fireParticles.Play();
+        StartCoroutine(Attack());
+
+    }
+
+    IEnumerator Attack()
+    {
+        for (int i = 0; i < attackCount; i++)
+        {
+            var projectile = Runner.Spawn(bulletPrefab, firePoint.position, firePoint.rotation, Object.InputAuthority);
+            projectile.ownerEnemy = this;
+            projectile.Fire(firePoint.position, firePoint.rotation, enemyData.damage, raycastLayerMask, targetAttribute);
+            fireParticles.Play();
+            yield return new WaitForSeconds(0.4f);
+        }
     }
 }
